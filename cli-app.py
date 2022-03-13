8c5022ee03499a5b189d7d0d95a803fe96d0eff3
sabir01 <sabir.cse5.bu@gmail.com>
2022-03-13 20:55:21 +0600

    feat: add user friendly cli input flags

--- cli-app.py
+++ cli-app.py
@@ -1,25 +1,29 @@
-import cv2
-from rich import print as rprint
-from rich.console import Console
 import os
-import time 
+import time
+import sys
+import cv2
+
+from rich.console import Console
+
 def clearConsole():
     command = 'clear'
     if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
         command = 'cls'
     os.system(command)
 
+
 def getASCII_Color(color):
-    density = "Ñ@#W$9876543210?!abc;:+=-,._   ";
+    density = "Ñ@#W$9876543210?!abc;:+=-,._   "
     density = density[::-1]
 
-    density = '       .:-i|=+%O#@'
+    density = '       .:-i|=+%\O#@'
     return density[int(color/len(density))]
 
 
 def convertFrameGray(image, w, h):
+    clearConsole()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    result = cv2.resize(gray, (w, h), interpolation= cv2.INTER_LINEAR)
+    result = cv2.resize(gray, (w, h), interpolation=cv2.INTER_LINEAR)
     result = cv2.flip(result, 1)
 
     for rows in result:
@@ -27,55 +31,69 @@ def convertFrameGray(image, w, h):
             print(getASCII_Color(pixel), end='')
         print()
     time.sleep(0.25)
+
+
+def convertFrameColor(image, w, h, ch='a'):
     clearConsole()
-
-
-def convertFrameColor(image, w, h):
     console = Console()
 
-    result = cv2.resize(image, (w, h), interpolation= cv2.INTER_LINEAR)
-    cv2.flip(result, 1)
+    result = cv2.resize(image, (w, h), interpolation=cv2.INTER_LINEAR)
+    result = cv2.flip(result, 1)
     for rows in result:
         for pixel in rows:
             gray_pixel = 0
             for val in pixel:
                 gray_pixel += val
             gray_pixel = gray_pixel / 3
-            b = pixel[0] 
-            g = pixel[1] 
-            r = pixel[2] 
+            b = pixel[0]
+            g = pixel[1]
+            r = pixel[2]
             pixel_color = "rgb("+str(r)+","+str(g)+","+str(b)+")"
 
-            console.print(getASCII_Color(gray_pixel), end='', style=pixel_color)
+            console.print(ch, end='', style=pixel_color)
+            #console.print(getASCII_Color(gray_pixel), end='', style=pixel_color)
         print()
-    time.sleep(0.23)
-    clearConsole()
+
+    time.sleep(0.35)
 
 
-def AsciiWebcam():
+def AsciiWebcam(arg, ch):
     cam = cv2.VideoCapture(0)
     while True:
         check, frame = cam.read()
-
         #cv2.imshow('webcam feed', frame)
-
         size = os.get_terminal_size()
-        convertFrameGray(frame, size[0], size[1])
-        #convertFrameColor(frame, size[0], size[1])
-
+        if arg == '-c' or arg == 'color' or arg == 'col' or arg == '--color' or arg == '--c' or arg == '-color':
+            convertFrameColor(frame, size[0], size[1], ch=ch)
+        else:
+            convertFrameGray(frame, size[0], size[1])
 
         key = cv2.waitKey(1)
         if key == 27:
             break
 
     cam.release()
-    #cv2.destroyAllWindows()
+    # cv2.destroyAllWindows( )
 
 def main():
-    #og_image = cv2.imread("ss.png")
-    AsciiWebcam()
-    #convertFrameColor(og_image)
+    arg = "----"
+    second = 'a'
+    if len(sys.argv) >= 3:
+        second = sys.argv[2]
+    if len(sys.argv) >= 2:
+        arg = sys.argv[1]
 
+    if arg == 'image' or arg == '--image' or arg == '-image':
+        if second != 'a':
+            size = os.get_terminal_size()
+            img = cv2.imread(second)
+            convertFrameColor(img, size[0], size[1])
+        else:
+            print("Error: No file input");
+
+    else:
+        clearConsole()
+        AsciiWebcam(arg, second)
 
 if __name__ == '__main__':
     main()
